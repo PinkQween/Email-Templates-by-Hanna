6823c78925c9a657a746cf1a3765384b247db3f5
Hanna Skairipa <83796834+PinkQween@users.noreply.github.com>
2024-03-16 09:18:43 -0600

    Initial Commit

---
+++ Email Templates by Hanna/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Email Templates by Hanna
+//
+//  Created by Boone, Hanna - Student on 3/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Email Templates by Hanna/Email_Templates_by_HannaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Email_Templates_by_HannaApp.swift
+//  Email Templates by Hanna
+//
+//  Created by Boone, Hanna - Student on 3/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Email_Templates_by_HannaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
